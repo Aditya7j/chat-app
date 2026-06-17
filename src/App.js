ea097df80423a377fd04bf628bc0dbdfb47c93b7
Aditya Kumar <aditya.kumar@BOLDINDIA.COM>
2026-06-17 21:13:12 +0530

    chat-app theme based CSS changes added across file

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import AppRoutes from "./routes/AppRoutes";
 import { useEffect, useContext } from "react";
 import socket from "./services/socket";
 import { ChatContext } from "./context/ChatContext";
+import { ThemeProvider } from "./context/ThemeContext";
 import "./styles/global.css";
 import { Toaster } from "react-hot-toast";
 
 function App() {
-
   const { setOnlineUsers } = useContext(ChatContext);
 
   useEffect(() => {
     const sendSetup = () => {
-
-      const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+      const userInfo = JSON.parse(
+        localStorage.getItem("userInfo")
+      );
 
       if (userInfo) {
-        socket.emit("setup", userInfo);
+        socket.emit(
+          "setup",
+          userInfo
+        );
       }
     };
 
@@ -23,38 +27,60 @@ function App() {
       sendSetup();
     };
 
-    const handleOnlineUsers = (users) => {
+    const handleOnlineUsers = (
+      users
+    ) => {
       setOnlineUsers(users);
     };
 
-    socket.on("connect", handleConnect);
+    socket.on(
+      "connect",
+      handleConnect
+    );
 
-    socket.on("online users", handleOnlineUsers);
+    socket.on(
+      "online users",
+      handleOnlineUsers
+    );
 
     if (socket.connected) {
       handleConnect();
     }
 
-    window.addEventListener("focus", sendSetup);
+    window.addEventListener(
+      "focus",
+      sendSetup
+    );
 
     return () => {
-      socket.off("connect", handleConnect);
-      socket.off("online users", handleOnlineUsers);
-      window.removeEventListener("focus", sendSetup);
-    };
+      socket.off(
+        "connect",
+        handleConnect
+      );
 
+      socket.off(
+        "online users",
+        handleOnlineUsers
+      );
+
+      window.removeEventListener(
+        "focus",
+        sendSetup
+      );
+    };
   }, [setOnlineUsers]);
 
   return (
-    <div>
+    <ThemeProvider>
       <Toaster
         position="top-right"
         toastOptions={{
           duration: 3000,
         }}
       />
+
       <AppRoutes />
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,13 +1,15 @@
 import "../styles/sidebar.css";
-import { FiMessageSquare, FiUsers, FiSettings, FiHome } from "react-icons/fi";
+import { FiMessageSquare, FiUsers, FiSettings, FiHome, FiSun, FiMoon } from "react-icons/fi";
 import { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import ProfileDrawer from "./ProfileDrawer";
+import { ThemeContext } from "../context/ThemeContext";
 
 const Sidebar = () => {
 
     const { user } = useContext(AuthContext);
     const [openProfile, setOpenProfile] = useState(false);
+    const { theme, toggleTheme } = useContext(ThemeContext);
 
     return (
         <>
@@ -28,6 +30,15 @@ const Sidebar = () => {
 
                     <button className="menu-btn">
                         <FiSettings />
+                    </button>
+
+                    <button
+                        className="menu-btn"
+                        onClick={toggleTheme}
+                    >
+                        {theme === "dark"
+                            ? <FiSun />
+                            : <FiMoon />}
                     </button>
 
                 </div>
